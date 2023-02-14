b6ce6af5e05665b6382bb6d7f8494b9dad79ffd5
wakisaka302 <natirouga32@gmail.com>
2023-02-14 15:57:34 +0900

    ユーザー情報のeditページ追加、UserUpdateRequestとりあえず編集しました。

--- src/main/java/com/example/demo/dto/UserUpdateRequest.java
+++ src/main/java/com/example/demo/dto/UserUpdateRequest.java
@@ -1,6 +1,9 @@
 package com.example.demo.dto;
 import java.io.Serializable;
-import jakarta.validation.constraints.NotNull;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 /**
@@ -10,9 +13,35 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class UserUpdateRequest extends UserRequest implements Serializable {
-  /**
-   * ユーザーID
-   */
-  @NotNull
-  private Long id;
+	/**
+	   * 名前
+	   */
+	  @NotEmpty(message = "名前を入力してください")
+	  @Size(max = 100, message = "名前は100桁以内で入力してください")
+	  private String name;
+	  /**
+	   * 生年月日
+	   */
+	  @Size(max = 8, message = "誕生日は8桁以内で入力してください")
+	  private String birth;
+	  /**
+	   * 住所
+	   */
+	  @Size(max = 8, message = "住所は100桁以内で入力してください")
+	  private String address;
+	  /**
+	   * メールアドレス
+	   */
+	  @Size(max = 8, message = "メールアドレスは100桁以内で入力してください")
+	  private String mail;
+	  /**
+	   * クレジット番号
+	   */
+	  @Pattern(regexp = "0\\d{1,4}-\\d{1,4}-\\d{1,4}-\\d{4}", message = "クレジット番号の形式で入力してください")
+	  private String credit;
+	  /**
+	   * パスワード
+	   */
+	  @Size(max = 8, message = "パスワードは8桁以内で入力してください")
+	  private String pw;
 }
