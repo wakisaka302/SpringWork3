91de3c38c2d09dfc39f9bed2af0c020983f112e8
3030742 <3030742@30307-42>
2023-02-17 17:42:25 +0900

    123

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.dto.UserRequest;
 
 @Controller
 public class UserController {
@@ -47,8 +58,17 @@ public class UserController {
 	 */
 	@GetMapping(value = "/user/{id}")
 	public String displayView(@PathVariable Long id, Model model) {
-//		Userinfo user = UserService.findById(id);
+//		Userinfo user = UserService.getUserInfo(id);
+//      if(user.userid =!= null){
 //		model.addAttribute("userData", user);
+		// List<Sales> list = UserService.getSales(id)
+		//model.addAttribute("userlist", list);
+		//return "user/view";
+		
+//      } else{
+//		return "goods/top";
+//	}
+//		
 		return "user/view";
 	}
 
@@ -59,8 +79,7 @@ public class UserController {
 	 */
 	@GetMapping(value = "/user/login")
 	public String displayLogin(Model model) {
-//		List<UserInfo> userlist = UserService.searchAll();
-//		model.addAttribute("userlist", userlist);
+
 		return "user/login";
 	}
 
@@ -71,8 +90,7 @@ public class UserController {
 	 */
 	@GetMapping(value = "/company/companyinfo")
 	public String displayCompany(Model model) {
-//		List<User> userlist = UserService.searchAll();
-//		model.addAttribute("userlist", userlist);
+
 		return "company/companyinfo";
 	}
 
@@ -83,21 +101,8 @@ public class UserController {
 	 */
 	@GetMapping(value = "/company/Q&A")
 	public String displayQa(Model model) {
-//		List<User> userlist = UserService.searchAll();
-//		model.addAttribute("userlist", userlist);
-		return "company/Q&A";
-	}
 
-	/**
-	 * 商品検索結果一覧画面を表示
-	 * @param model Model
-	 * @return 検索結果一覧画面
-	 */
-	@GetMapping(value = "/goods/search")
-	public String displaySearch(Model model) {
-//		List<Goods> userlist = UserService.searchAll();
-//		model.addAttribute("userlist", userlist);
-		return "goods/search";
+		return "company/Q&A";
 	}
 
 
@@ -115,6 +120,26 @@ public class UserController {
 		return "user/add";
 	}
 	
-	  
+	/**
+	 * ユーザー新規登録
+	 * @param userRequest リクエストデータ
+	 * @param model Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@RequestMapping(value = "/user/create", method = RequestMethod.POST)
+	public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			// 入力チェックエラーの場合
+		List<String> errorList = new ArrayList<String>();
+			for (ObjectError error : result.getAllErrors()) {
+				errorList.add(error.getDefaultMessage());
+			}
+			model.addAttribute("validationError", errorList);
+			return "user/add";
+		}
+		// ユーザー情報の登録
+		//UserService.userEntry(UserRequest);
+		return "redirect:/goods/top";
+	}
 
 }
